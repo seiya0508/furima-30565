b9f907b1ab2eaad59018274b9b633ca4335576ae
seiya0508 <71762860+seiya0508@users.noreply.github.com>
2020-10-23 16:14:27 +0900

    with_optionsでバリデーションをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :buyers
-
+  
+  with_options presence: true do
   validates :nickname, presence: true
   validates :birthday, presence: true
-  validates :first_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }, presence: true
-  validates :last_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }, presence: true
-  validates :first_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/ }, presence: true
-  validates :last_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/ }, presence: true
+  validates :first_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }
+  validates :last_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }
+  validates :first_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/ }
+  validates :last_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/ }
+  end
 end
