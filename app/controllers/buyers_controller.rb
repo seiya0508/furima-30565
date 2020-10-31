c7239d637698e983229a6ffa8d6abcb7215b0243
seiya0508 <71762860+seiya0508@users.noreply.github.com>
2020-10-31 13:24:13 +0900

    formオブジェクト配送情報

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -3,12 +3,26 @@ class BuyersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @buyer_form = BuyerForm.new
   end
 
   def new
   end
 
   def create
+    @buyer_form = BuyerForm.new(buyer_params)
+    binding.pry
+    if @buyer_form.valid?
+      @buyer_form.save
+      redirect_to root_path
+    else
+      render action: :index
+  end
+end
+ 
+  private
+  def buyer_params
+    params.require(:buyer_form).permit(:postal_code, :prefecture_id, :municipality, :address, :tel, :building)
   end
 
 end
